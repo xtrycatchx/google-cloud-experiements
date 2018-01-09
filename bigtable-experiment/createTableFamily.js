ae3ae604b7aa729bea4946ac01f68465e24f781b
Paul Sydney Orozco <xtrycatchx@gmail.com>
2018-01-09 22:46:56 +0800

    minor cleanup

--- bigtable-experiment/createTableFamily.js
+++ bigtable-experiment/createTableFamily.js
@@ -7,7 +7,7 @@ const instanceName = 'sydney-instance';
 const instance = bigtable.instance(instanceName);
 const table = instance.table('tablexyz');
 
-table.createFamily('yawe', function (err, family) {
+table.createFamily('yawe',  (err, family) =>{
     if (err) {
         console.log(`error creating family ${err}`)
     } else {

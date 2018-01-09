f7c8cbd8c3f508e079a1a0aa5b02b10b3c3916b5
Paul Sydney Orozco <xtrycatchx@gmail.com>
2018-01-09 22:46:20 +0800

    minor cleanup

--- bigtable-experiment/createTableFamily.js
+++ bigtable-experiment/createTableFamily.js
@@ -4,7 +4,7 @@ const bigtable = Bigtable({
     keyFilename: '../keyfile.json'
 });
 const instanceName = 'sydney-instance';
-var instance = bigtable.instance(instanceName);
+const instance = bigtable.instance(instanceName);
 var table = instance.table('tablexyz');
 
 table.createFamily('yawe', function (err, family) {

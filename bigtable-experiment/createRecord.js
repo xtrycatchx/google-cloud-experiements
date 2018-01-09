6135f023486a4bebe40a66fce14a90f733859f79
Paul Sydney Orozco <xtrycatchx@gmail.com>
2018-01-09 22:42:01 +0800

    minor cleanup

--- bigtable-experiment/createRecord.js
+++ bigtable-experiment/createRecord.js
@@ -4,8 +4,8 @@ const bigtable = Bigtable({
     keyFilename: '../keyfile.json'
 });
 const instanceName = 'sydney-instance';
-var instance = bigtable.instance(instanceName);
-var table = instance.table('tablexyz');
+const instance = bigtable.instance(instanceName);
+const table = instance.table('tablexyz');
 
 table.getRows(function (err, rows) {
     if (err) {
